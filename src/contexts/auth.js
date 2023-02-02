fe71078721ec07c186294a7b6795fbd16d6f92d1
José Victor <jvictorfrance@outlook.com>
2023-02-02 01:18:52 -0300

    Login screen finished

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,72 +1,74 @@
-import { createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext({});
 
-export const AuthProvider = ({ children}) => {
-    const [user, setUser] = useState();
-    
-    useEffect(() => {
-        const userToken = localStorage.getItem("user_token");
-        const usersStorage = localStorage.getItem("users_db");
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
 
-        if (userToken && usersStorage){
-            const hasUser = JSON.parse(usersStorage)?.filter(
-                (user) => user.email === JSON.parse(userToken).email
-            );
-            if(hasUser) setUser(hasUser[0]);
-        }
-    }, []);
+  useEffect(() => {
+    const userToken = localStorage.getItem("user_token");
+    const usersStorage = localStorage.getItem("users_bd");
 
-    const signin = (email, password) => {
-        const usersStorage = JSON.parse(localStorage.getItem("users_db"));
-        const hasUser = usersStorage?.filter((user) => user.email === email);
+    if (userToken && usersStorage) {
+      const hasUser = JSON.parse(usersStorage)?.filter(
+        (user) => user.email === JSON.parse(userToken).email
+      );
 
-        if (hasUser?.lenght) {
-            if (hasUser[0].email === email && hasUser[0].password == password){
-                const token = Math.random().toString(36).substring(2);
-                localStorage.setItem("user_token", JSON.stringify({ email, token}));
-                setUser({ email, password});
-                return;
-            }else {
-                return "E-mail ou senha incorretos";
-            }
-        }else {
-            return "Usuário não cadastrado";
-        }
-    };
+      if (hasUser) setUser(hasUser[0]);
+    }
+  }, []);
 
-    const signup = (email, password) => {
-        const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
-        const hasUser = usersStorage?.filter((user) => user.email === email);
+  const signin = (email, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
 
-        if (hasUser?.lenght){
-            return "Já existe uma conta com esse E-mail";
-        }
+    const hasUser = usersStorage?.filter((user) => user.email === email);
 
-        let newUser;
-
-        if(usersStorage) {
-            newUser = [...usersStorage, { email, password}];
-        }else {
-            newUser = [{ email, password}];
-        }
-
-        localStorage.setItem("users_bd", JSON.stringify(newUser));
-
+    if (hasUser?.length) {
+      if (hasUser[0].email === email && hasUser[0].password === password) {
+        const token = Math.random().toString(36).substring(2);
+        localStorage.setItem("user_token", JSON.stringify({ email, token }));
+        setUser({ email, password });
         return;
-    };
+      } else {
+        return "E-mail ou senha incorretos";
+      }
+    } else {
+      return "Usuário não cadastrado";
+    }
+  };
 
-    const signout = () => {
-        setUser(null);
-        localStorage.removeItem("user_token");
-    };
+  const signup = (email, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"));
 
-    return (
-        <AuthContext.Provider
-        value={{ user, signed: !!user, signin, signup, signout}}
-        >
-            {children}
+    const hasUser = usersStorage?.filter((user) => user.email === email);
 
-        </AuthContext.Provider>
-    );
+    if (hasUser?.length) {
+      return "Já tem uma conta com esse E-mail";
+    }
+
+    let newUser;
+
+    if (usersStorage) {
+      newUser = [...usersStorage, { email, password }];
+    } else {
+      newUser = [{ email, password }];
+    }
+
+    localStorage.setItem("users_bd", JSON.stringify(newUser));
+
+    return;
+  };
+
+  const signout = () => {
+    setUser(null);
+    localStorage.removeItem("user_token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signed: !!user, signin, signup, signout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,32 @@
 import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/home';
-import Signin from "../pages/signin"
+import Signin from "../pages/signin";
 import Signup from '../pages/signup';
+import useAuth from "../hooks/useAuth";
 
 
 const Private = ({ Item }) => {
-    const signed = false;
+    const { signed } = useAuth();
 
     return signed > 0 ? <Item /> : <Signin />;
 }
 
 
 const RoutesApp = () => {
-    return (
-        <BrowserRouter>
-        <Fragment>
-            <Routes>
-                <Route exact path= "/home" element={<Private item={Home}/>} />
-                <Route path= "/" element={<Signin/>} />
-                <Route exact path= "/signup" element={<Signup/>} />
-                <Route path= "*" element={<Signin/>} />
-                
-            </Routes>
-        </Fragment>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <Routes>
+          <Route exact path="/home" element={<Private Item={Home} />} />
+          <Route path="/" element={<Signin />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route path="*" element={<Signin />} />
+        </Routes>
+      </Fragment>
+    </BrowserRouter>
+  );
+
 };
 
 export default RoutesApp;
